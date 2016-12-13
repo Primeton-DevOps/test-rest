07d82a80e363ad8cf4b66bf3be2a4dca0bebf54d
ZhongWen Li <lizhongwen1989@gmail.com>
2016-12-13 23:11:21 +0800

    Update

--- src/test/resources/mysql/platform-template.sql
+++ src/test/resources/mysql/platform-template.sql
@@ -1,4 +1,7 @@
-INSERT INTO `dps_dlv_platform_template` (`PLATFORM_TP_ID`, `PLATFORM_TP_NAME`, `PLATFORM_TP_TYPE`, `DESCRIPTION`, `CREATE_USER`, `CREATE_TIME`, `UPDATE_USER`, `UPDATE_TIME`, `TENANT_ID`) VALUES ('1', 'mysql', 'RDB', 'mysql', 'sysadmin', now(), 'sysadmin', now(), 'sysadmin');
-INSERT INTO `dps_dlv_platform_template` (`PLATFORM_TP_ID`, `PLATFORM_TP_NAME`, `PLATFORM_TP_TYPE`, `DESCRIPTION`, `CREATE_USER`, `CREATE_TIME`, `UPDATE_USER`, `UPDATE_TIME`, `TENANT_ID`) VALUES ('2', 'tomcat', 'J2EE', 'tomcat', 'sysadmin', now(), 'sysadmin', now(), 'sysadmin');
-INSERT INTO `dps_dlv_platform_template` (`PLATFORM_TP_ID`, `PLATFORM_TP_NAME`, `PLATFORM_TP_TYPE`, `DESCRIPTION`, `CREATE_USER`, `CREATE_TIME`, `UPDATE_USER`, `UPDATE_TIME`, `TENANT_ID`) VALUES ('3', 'redis', 'NoSQL', 'redis', 'sysadmin', now(), 'sysadmin', now(), 'sysadmin');
+INSERT INTO `dps_dlv_platform_template` (`PLATFORM_TP_ID`, `PLATFORM_TP_NAME`, `PLATFORM_TP_TYPE`, `DESCRIPTION`, `CREATE_USER`, `CREATE_TIME`, `UPDATE_USER`, `UPDATE_TIME`, `TENANT_ID`) 
+	VALUES ('1', 'mysql', 'RDB', 'mysql', 'sysadmin', now(), 'sysadmin', now(), 'sysadmin');
+INSERT INTO `dps_dlv_platform_template` (`PLATFORM_TP_ID`, `PLATFORM_TP_NAME`, `PLATFORM_TP_TYPE`, `DESCRIPTION`, `CREATE_USER`, `CREATE_TIME`, `UPDATE_USER`, `UPDATE_TIME`, `TENANT_ID`) 
+	VALUES ('2', 'tomcat', 'J2EE', 'tomcat', 'sysadmin', now(), 'sysadmin', now(), 'sysadmin');
+INSERT INTO `dps_dlv_platform_template` (`PLATFORM_TP_ID`, `PLATFORM_TP_NAME`, `PLATFORM_TP_TYPE`, `DESCRIPTION`, `CREATE_USER`, `CREATE_TIME`, `UPDATE_USER`, `UPDATE_TIME`, `TENANT_ID`) 
+	VALUES ('3', 'redis', 'NoSQL', 'redis', 'sysadmin', now(), 'sysadmin', now(), 'sysadmin');
 
