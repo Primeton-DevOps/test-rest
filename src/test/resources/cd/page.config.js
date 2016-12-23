4aeb74111420253b5b492d4a63987e0ad226d899
lizhongwen1989 <lizhongwen1989@gmail.com>
2016-12-23 17:57:34 +0800

    Update page.config.js

--- src/test/resources/cd/page.config.js
+++ src/test/resources/cd/page.config.js
@@ -1,151 +1,82 @@
 var pages = [{
-  id: "portal", // 一级页面标示符
-  urlPath: "portal", // 浏览器路由地址,  localhost:8080/default/index.html/#/portal
-  page: "sample/pages/portal/index.html", // HTML文件目录，相对于webcontent的路径，如：devops-sample/com.primeton.devops.sample/src/webcontent/pages/portal/index.html, 注意别忘了加构建包的webPath
-  root: true
-}, {
-  id: "signup",
-  urlPath: "signup",
-  page: "sample/pages/portal/signup.html"
-}, {
-  id: "forgotPassword",
-  urlPath: "forgotPassword",
-  page: "sample/pages/portal/forgotPassword.html"
-}, {
-  id: "signin",
-  urlPath: "signin",
-  page: "sample/pages/portal/signin.html"
-}, {
-  id: "apply",
-  urlPath: "apply",
-  page: "sample/pages/portal/apply.html"
-}, {
-  id: "home",
-  urlPath: "home",
-  pages: [{
-    id: "form", // 二级页面标示符
-    urlPath: "form", // 浏览器路由地址， localhost:8080/default/index.html/#/home/form
-    name: "Form",
-    icon: "assets/images/home/product.svg",
-    pages: [{
-    	id: "formBasic", // 三级页面标示符
-    	urlPath: "basic", // 浏览器路由地址， localhost:8080/default/index.html/#/home/form/basic
-    	page: "sample/pages/home/form/index.html",
-    	name: "Form"
+  id : "project",
+  urlPath : "project/:projectId",
+  pages : [{
+    id : "build",
+    name : "构建",
+    urlPath : "build",
+    pages : [{
+      id : "design",
+      name : "设计",
+      urlPath : "design",
+      page : "cd/build/design.html"
     }, {
-    	id: "formTooltip",
-    	urlPath: "tooltip",
-    	page: "sample/pages/home/form/toolTip.html",
-    	name: "Tooltip"
-    }, {
-    	id: "formButton",
-    	urlPath: "button",
-    	page: "sample/pages/home/form/button.html",
-    	name: "Button"
-    }, {
-    	id: "formCombobox",
-    	urlPath: "combobox",
-    	page: "sample/pages/home/form/comboBox.html",
-    	name: "ComboBox"
-    }, {
-    	id: "formIframe",
-    	urlPath: "iframe",
-    	frame: true,
-        page: "sample/pages/home/form/iframe.html",
-    	name: "Iframe"
+      id : "browse",
+      name : "浏览",
+      urlPath : "browse",
+      page : "cd/build/browse.html"
     }]
   }, {
-    id: "tree",
-    urlPath: "tree",
-    name: "Tree",
-    icon: "assets/images/home/market.svg",
-    pages: [{
-    	id: "treeBasic",
-    	urlPath: "basic",
-    	page: "sample/pages/home/tree/index.html",
-    	name: "Tree"
+    id : "deployment",
+    urlPath : "deploy",
+    name : "部署",
+    icon : "assets/images/home/ziyuan.svg",
+    pages : [{
+      id : "assemblymgr",
+      urlPath : "assemblymgr",
+      page : "cd/deploy/assembly/browse.html",
+      name : "部署装配"
     }, {
-    	id: "treeFiltertree",
-    	urlPath: "filtertree",
-    	page: "sample/pages/home/tree/filterTree.html",
-    	name: "FilterTree"
-    },{
-    	id: "contextMenutree",
-    	urlPath: "contextmenu",
-    	page: "sample/pages/home/tree/contextMenu.html",
-    	name: "ContextMenu"
+      id : "gridDatabinding",
+      urlPath : "databinding",
+      page : "cd/deploy/platform/overview.html",
+      name : "云服务"
+    }, {
+      id : "assemblies",
+      urlPath : "assemblies",
+      navbar : false,
+      pages : [{
+        id : "assembly_overview",
+        urlPath : ":assemblyid/overview",
+        page : "cd/deploy/platform/overview.html"
+      }]
+    }, {
+      id : "platforms",
+      urlPath : "platforms",
+      navbar : false,
+      pages : [{
+        id : "platform_overview",
+        urlPath : ":platformid/overview",
+        page : "cd/deploy/component/overview.html"
+      }]
     }]
   }, {
-    id: "dnd",
-    urlPath: "dnd",
-    name: "Drag&Drop",
-    icon: "assets/images/home/product.svg",
-    page:  "sample/pages/home/dnd/index.html" 
-  }, {
-    id: "echart",
-    urlPath: "echarts",
-    name: "Chart_ECharts",
-    icon: "assets/images/home/market.svg",
-    page:"sample/pages/home/chart/basicCharts.html" 
-  }, {
-    id: "tabs",
-    urlPath: "tabs",
-    name: "Tabs",
-    icon: "assets/images/home/product.svg",
-    page: "sample/pages/home/tabs/index.html"
-    },{
-    id: "grid",
-    urlPath: "grid",
-    name: "Grid",
-    icon: "assets/images/home/ziyuan.svg",
-    pages: [{
-      id: "gridBasic",
-      urlPath: "basic",
-      page: "sample/pages/home/grid/index.html",
-      name: "Grid"
+    id : "project_admin",
+    urlPath : "admin",
+    name : "管理",
+    pages : [{
+      id : "project_team",
+      name : "团队",
+      urlPath : "team",
+      page : "pm/project/admin/team.html",
+      icon : "assets/images/home/ziyuan.svg"
     }, {
-      id: "gridDatabinding",
-      urlPath: "databinding",
-      page: "sample/pages/home/grid/dataBinding.html",
-      name: "DataBinding"
-  }]
-},{
-    id: "deploy",
-    urlPath: "deploy",
-    name: "部署",
-    icon: "assets/images/home/ziyuan.svg",
-    pages: [{
-      id: "assembly",
-      urlPath: "assembly",
-      page: "cd/deploy/html/assembly/browse.html",
-      name: "部署装配"
+      id : "project_role",
+      name : "角色权限",
+      urlPath : "role",
+      page : "pm/project/admin/role.html",
+      icon : "assets/images/home/ziyuan.svg"
     }, {
-      id: "gridDatabinding",
-      urlPath: "databinding",
-      page: "cd/deploy/assembly_platform.html",
-      name: "云服务"
-  }]
-}]
-}, {
-  id: "assemblies",
-  urlPath: "assemblies",
-  navbar: false,
-  pages: [{
-    id: "assembly_preview",
-    urlPath: ":assemblyid/preview",
-    page: "cd/deploy/assembly_platform.html"
-  }]
-}, {
-  id: "platforms",
-  urlPath: "platforms",
-  navbar: false,
-  pages: [{
-    id : "platform_preview",
-    urlPath : ":platformid/preview",
-    page: "cd/deploy/platform_component.html"
+      id : "codeRepository",
+      name : "代码库",
+      urlPath : "coderep",
+      page : "pm/project/admin/role.html"
+    }]
   }]
 }];
+
 var config = {
-  pages: pages
+  pages : pages
 };
+
 module.exports = config;
